6a0c40e719df5056bf6043a09aa98265b7180db5
RoninNunisa <144159717+RoninNunisa@users.noreply.github.com>
2024-07-23 23:54:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { PageHeader } from "./layout/PageHeader";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      <div className="max-h-screen flex flex-col">
+        <PageHeader></PageHeader>
+        <div> 2 </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+
+
+import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import clsx from 'clsx'; // Import clsx to help with className merging
+
+const buttonStyle = cva(["transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
+    },
+    size: {
+      default: ["rounded", "p-2"],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "items-center",
+        "justify-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type ButtonProps = VariantProps<typeof buttonStyle> & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({ variant, size, className, ...props }: ButtonProps) {
+  return (
+    <button className={clsx(buttonStyle({ variant, size }), className)} {...props}></button>
+  );
+}
